63749ab355a8773ffda978f1cc2d14e848601320
Adiks <apacholak97@interia.pl>
2023-09-24 09:50:02 +0200

    adding new user-service (initial)

---
+++ user-service/src/main/java/pl/adrianpacholak/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.adrianpacholak.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/pl/adrianpacholak/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adrianpacholak.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
