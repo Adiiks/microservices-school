deb6517c7aa3220a0bb26b8fdc6f682de170407c
Adiks <apacholak97@interia.pl>
2023-10-06 11:22:50 +0200

    adding new service: attendance-service

---
+++ attendance-service/src/main/java/pl/adrianpacholak/attendanceservice/AttendanceServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.adrianpacholak.attendanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceServiceApplication.class, args);
+	}
+
+}
